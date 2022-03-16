5e357c319a1d73e852786e431ef12b196c92536b
Shikha Saluja <shikha.c.saluja@gmail.com>
2022-03-16 09:26:35 -0700

    updated copyright

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
-/*
-Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+// Copyright 2021 Edgecast Inc., Licensed under the terms of the Apache 2.0 license.
+// See LICENSE file in project root for terms.
 
-*/
 package main
 
 import (
